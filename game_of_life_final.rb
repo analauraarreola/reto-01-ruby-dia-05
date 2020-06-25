f313758c2c6b22ffbf25dd5fbc6f906c4e54432a
Ana Arreola <laurarreola.ana@gmail.com>
2020-06-25 17:47:01 -0500

    Day 05 sesion 03 Algorithm without if, no loops and only 5 lines in each method

--- game_of_life_final.rb
+++ game_of_life_final.rb
@@ -14,26 +14,31 @@ class CreatesBoard
             #¿cuáles células están vivas?
         end
     end
+
+    def status
+        @board.include?(1) #Este sirve para evaluar las celulas vivas de cada posición
+    end
+
     def inside_board?(row,column)
         row < @size && row >= 0 && column < @size && column >= 0
          end
-         
+    
     def neighbours(row, column) #Analizando los vecinos de cada célula
     neighbours = []
     case neighbours
     when neighbours.push(@board[row-1, column])   == inside_board?(row-1, column)
     when neighbours.push(@board[row-1, column+1]) == inside_board?(row-1, column+1)
-    when neighbours.push(@board[row, column+1])   == inside_board?(row, column+1)
+    when neighbours.push(@board[row, column+1])   == inside_board?(row, column+1) 
     when neighbours.push(@board[row+1, column+1]) == inside_board?(row+1, column+1)
-    when neighbours.push(@board[row+1, column])   == inside_board?(row+1, column)
+    when neighbours.push(@board[row+1, column])   == inside_board?(row+1, column) 
     when neighbours.push(@board[row+1, column-1]) == inside_board?(row+1, column-1)
-    when neighbours.push(@board[row, column-1])   == inside_board?(row, column-1)
+    when neighbours.push(@board[row, column-1])   == inside_board?(row, column-1) 
     when neighbours.push(@board[row-1, column-1]) == inside_board?(row-1, column-1)
         neighbours.count(1)
-puts neighbours.count
     end
-end
+  end
 end
 new_board = CreatesBoard.new
 puts new_board.print_board
 new_board.iteration
+
